32e5677d6bd84f9e6c1bbb8e25cb05b17c08390d
Terence Bezman <terencebezman@gmail.com>
2014-05-08 20:09:17 -0400

    Init commit

---
+++ Lab03TB.java
@@ -0,0 +1,26 @@
+/**
+ * @(#)Lab03TB.java
+ *
+ *
+ * @author 
+ * @version 1.00 2013/9/30
+ */
+
+
+public class Lab03TB {
+    
+    public static void main(String[] args)
+    {
+    	final int NUM_THIRTEEN = 13;
+    	final int NUM_TWENTY_THREE = 23;
+    	
+    	System.out.println("Ten robins plus " + NUM_THIRTEEN + " canaries is " + NUM_TWENTY_THREE + " birds");	
+    }
+    
+    public void print(String s)
+    {
+    	System.out.println(s);
+    }
+    
+   
+}
